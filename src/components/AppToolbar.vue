a8b45b23417872e7ec74d4f8792fd9e862a9503c
daizhe <qidaizhe11@gmail.com>
2019-11-03 17:44:17 +0800

    init, from toolkit/vue-material-admin

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-app-bar color="primary" dark app>
+    <v-app-bar-nav-icon @click="handleDrawerToggle" />
+    <v-spacer />
+    <v-toolbar-items>
+      <v-btn text href="mailto:wangqiangshen@gmail.com">Hire Me</v-btn>
+      <v-btn icon href="https://github.com/tookit/vue-material-admin">
+        <v-icon class="fa-2x">fa-github</v-icon>
+      </v-btn>
+      <v-btn icon @click="handleFullScreen()">
+        <v-icon>fullscreen</v-icon>
+      </v-btn>
+      <v-menu
+        offset-y
+        origin="center center"
+        class="elelvation-1"
+        :nudge-bottom="14"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon text slot="activator" v-on="on">
+            <v-badge color="red" overlap>
+              <span slot="badge">3</span>
+              <v-icon medium>notifications</v-icon>
+            </v-badge>
+          </v-btn>
+        </template>
+        <notification-list></notification-list>
+      </v-menu>
+      <v-menu
+        offset-y
+        origin="center center"
+        :nudge-bottom="10"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon large text slot="activator" v-on="on">
+            <v-avatar size="30px">
+              <img src="/static/avatar/man_4.jpg" alt="Michael Wang" />
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-list class="pa-0">
+          <v-list-item
+            v-for="(item, index) in items"
+            :to="!item.href ? { name: item.name } : null"
+            :href="item.href"
+            @click="item.click"
+            ripple="ripple"
+            :disabled="item.disabled"
+            :target="item.target"
+            rel="noopener"
+            :key="index"
+          >
+            <v-list-item-action v-if="item.icon">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+<script>
+import NotificationList from '@/components/widgets/list/NotificationList'
+import Util from '@/util'
+export default {
+  name: 'AppToolbar',
+  components: {
+    NotificationList,
+  },
+  data() {
+    return {
+      items: [
+        {
+          icon: 'account_circle',
+          href: '#',
+          title: 'Profile',
+          click: this.handleProfile,
+        },
+        {
+          icon: 'settings',
+          href: '#',
+          title: 'Settings',
+          click: this.handleSetting,
+        },
+        {
+          icon: 'fullscreen_exit',
+          href: '#',
+          title: 'Logout',
+          click: this.handleLogut,
+        },
+      ],
+    }
+  },
+  computed: {
+    toolbarColor() {
+      return this.$vuetify.options.extra.mainNav
+    },
+  },
+  methods: {
+    handleDrawerToggle() {
+      this.$emit('side-icon-click')
+    },
+    handleFullScreen() {
+      Util.toggleFullScreen()
+    },
+    handleLogut() {
+      this.$router.push('/auth/login')
+    },
+    handleSetting() {},
+    handleProfile() {},
+  },
+  created() {},
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/router/config.js
@@ -0,0 +1,247 @@
+import { AuthLayout, DefaultLayout, ChatLayout } from '@/components/layouts'
+
+export const publicRoute = [
+  {
+    path: '*',
+    component: () =>
+      import(/* webpackChunkName: "errors-404" */ '@/views/error/NotFound.vue'),
+  },
+  {
+    path: '/auth',
+    component: AuthLayout,
+    meta: {
+      title: 'Login',
+    },
+    redirect: '/auth/login',
+    hidden: true,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        meta: {
+          title: 'Login',
+        },
+        component: () =>
+          import(/* webpackChunkName: "login" */ '@/views/auth/Login.vue'),
+      },
+    ],
+  },
+
+  {
+    path: '/404',
+    name: '404',
+    meta: {
+      title: 'Not Found',
+    },
+    component: () =>
+      import(/* webpackChunkName: "errors-404" */ '@/views/error/NotFound.vue'),
+  },
+
+  {
+    path: '/500',
+    name: '500',
+    meta: {
+      title: 'Server Error',
+    },
+    component: () =>
+      import(/* webpackChunkName: "errors-500" */ '@/views/error/Error.vue'),
+  },
+]
+
+export const protectedRoute = [
+  {
+    path: '/',
+    component: DefaultLayout,
+    meta: {
+      title: 'Home',
+      group: 'apps',
+      icon: '',
+    },
+    redirect: '/dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        meta: {
+          title: 'Home',
+          group: 'apps',
+          icon: 'dashboard',
+        },
+        component: () =>
+          import(/* webpackChunkName: "dashboard" */ '@/views/Dashboard.vue'),
+      },
+      {
+        path: '/media',
+        meta: {
+          title: 'Media',
+          group: 'apps',
+          icon: 'media',
+        },
+        name: 'Media',
+        props: route => ({
+          type: route.query.type,
+        }),
+        component: () =>
+          import(/* webpackChunkName: "media" */ '@/views/Media.vue'),
+      },
+
+      {
+        path: '/task',
+        meta: {
+          title: 'Task',
+          group: 'apps',
+          icon: 'media',
+        },
+        name: 'Task',
+        props: route => ({
+          type: route.query.type,
+        }),
+        component: () =>
+          import(/* webpackChunkName: "task" */ '@/views/Calendar.vue'),
+      },
+      {
+        path: '/403',
+        name: 'Forbidden',
+        meta: {
+          title: 'Access Denied',
+          hiddenInMenu: true,
+        },
+        component: () =>
+          import(/* webpackChunkName: "error-403" */ '@/views/error/Deny.vue'),
+      },
+    ],
+  },
+
+  //list
+  {
+    path: '/cms',
+    component: DefaultLayout,
+    redirect: '/cms/table',
+    meta: {
+      title: 'CMS',
+      icon: 'view_compact',
+      group: 'cms',
+    },
+    children: [
+      {
+        path: '/cms/table',
+        name: 'ListTable',
+        meta: {
+          title: 'CMS Table',
+        },
+        component: () =>
+          import(/* webpackChunkName: "table" */ '@/views/list/Table.vue'),
+      },
+    ],
+  },
+
+  //widgets
+  {
+    path: '/widgets',
+    component: DefaultLayout,
+    meta: {
+      title: 'Widget',
+      icon: 'widgets',
+      group: 'advance',
+    },
+    redirect: '/widgets/chart',
+    children: [
+      {
+        path: '/widgets/chart',
+        name: 'ChartWidget',
+        meta: {
+          title: 'Chart Widget',
+        },
+        component: () =>
+          import(/* webpackChunkName: "chart-widget" */ '@/views/widgets/Chart.vue'),
+      },
+      {
+        path: '/widgets/list',
+        name: 'ListWidget',
+        meta: {
+          title: 'List Widget',
+        },
+        component: () =>
+          import(/* webpackChunkName: "list-widget" */ '@/views/widgets/List.vue'),
+      },
+      {
+        path: '/widgets/social',
+        name: 'SocialWidget',
+        meta: {
+          title: 'Social Widget',
+        },
+        component: () =>
+          import(/* webpackChunkName: "social-widget" */ '@/views/widgets/Social.vue'),
+      },
+      {
+        path: '/widgets/statistic',
+        name: 'StatisticWidget',
+        meta: {
+          title: 'Statistic Widget',
+        },
+        component: () =>
+          import(/* webpackChunkName: "statistic-widget" */ '@/views/widgets/Statistic.vue'),
+      },
+    ],
+  },
+
+  //media
+
+  // chat app
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: ChatLayout,
+    redirect: {
+      path: '/chat/messaging',
+    },
+    meta: {
+      title: 'Chat',
+      group: 'apps',
+      icon: 'chat_bubble',
+    },
+    children: [
+      {
+        path: '/chat/messaging/:uuid?',
+        name: 'ChatMessaging',
+        props: true,
+        component: () =>
+          import(/* webpackChunkName: "chat-messaging" */ '@/views/chat/ChatMessaging.vue'),
+      },
+      {
+        path: '/chat/contact/:uuid?',
+        meta: {
+          public: true,
+        },
+        name: 'ChatContact',
+        component: () =>
+          import(/* webpackChunkName: "chat-contact" */ '@/views/chat/ChatContact.vue'),
+      },
+    ],
+  },
+
+  //mail app
+  {
+    path: '/mail',
+    name: 'Mail',
+    component: () =>
+      import(/* webpackChunkName: "mail-layout" */ '@/components/email/Layout.vue'),
+    redirect: {
+      path: '/mail/all',
+    },
+    children: [
+      {
+        path: '/mail/:mailType',
+        name: 'MailIndex',
+        component: () =>
+          import(/* webpackChunkName: "mail-list" */ '@/components/email/List.vue'),
+      },
+      {
+        path: '/mail/0/:uuid',
+        name: 'MailDetail',
+        component: () =>
+          import(/* webpackChunkName: "mail-reply" */ '@/components/email/Reply.vue'),
+      },
+    ],
+  },
+]
